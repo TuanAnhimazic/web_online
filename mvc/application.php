3209b765bdcdbdfaa7351feabaed88934f90ab0f
TuanAnhimazic <95213649+TuanAnhimazic@users.noreply.github.com>
2022-05-01 08:22:06 +0700

    Update application.php

--- mvc/application.php
+++ mvc/application.php
@@ -4,6 +4,6 @@
     require_once "./mvc/core/database.php";
     require_once "./mvc/core/config.php";
     require_once "./mvc/core/notification.php";
-    require_once "mvc/core/randomcookie.php";
+    require_once "./mvc/core/randomcookie.php";
     require_once "./mvc/core/uploadfile.php";
 ?>
